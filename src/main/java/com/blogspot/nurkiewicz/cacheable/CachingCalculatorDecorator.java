10b895c0debd42e1b684fda6301c2b96e49a0f63
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-01-12 17:28:09 +0100

    Migrating CachingCalculatorDecorator from Scala to Java

---
+++ src/main/java/com/blogspot/nurkiewicz/cacheable/CachingCalculatorDecorator.java
@@ -0,0 +1,31 @@
+package com.blogspot.nurkiewicz.cacheable;
+
+import com.blogspot.nurkiewicz.cacheable.calculator.Calculator;
+
+import java.util.Map;
+
+/**
+ * @author Tomasz Nurkiewicz
+ * @since 1/12/13, 4:59 PM
+ */
+public class CachingCalculatorDecorator implements Calculator {
+
+	private final Map<Integer, Integer> cache = new java.util.concurrent.ConcurrentHashMap<Integer, Integer>();
+
+	private final Calculator target;
+
+	public CachingCalculatorDecorator(Calculator target) {
+		this.target = target;
+	}
+
+	@Override
+	public int identity(int x) {
+		final Integer existing = cache.get(x);
+		if (existing != null) {
+			return existing;
+		}
+		final int newValue = target.identity(x);
+		cache.put(x, newValue);
+		return newValue;
+	}
+}
