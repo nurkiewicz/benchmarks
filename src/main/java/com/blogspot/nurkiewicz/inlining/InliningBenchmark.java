cf32c4edaa973eb558caa24c3393971780cabd37
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-01-27 20:10:15 +0100

    Renaming DefaultAdder -> VirtualAdder

--- src/main/java/com/blogspot/nurkiewicz/inlining/InliningBenchmark.java
+++ src/main/java/com/blogspot/nurkiewicz/inlining/InliningBenchmark.java
@@ -1,9 +1,6 @@
 package com.blogspot.nurkiewicz.inlining;
 
 import com.google.caliper.SimpleBenchmark;
-import org.apache.commons.lang.time.DateUtils;
-
-import java.util.Date;
 
 /**
  * @author Tomasz Nurkiewicz
@@ -11,7 +8,7 @@ import java.util.Date;
  */
 public class InliningBenchmark extends SimpleBenchmark {
 
-	private final Adder adder = new DefaultAdder();
+	private final Adder virtualAdder = new VirtualAdder();
 	private final InlineAdder inlineAdder = new InlineAdder();
 	private final ConcreteAdder concreteAdder = new ConcreteAdder();
 
@@ -34,7 +31,7 @@ public class InliningBenchmark extends SimpleBenchmark {
 	public int timePlentyOfSmallVirtualMethodCalls(int reps) {
 		int accum = 0;
 		for (int i = 0; i < reps; ++i) {
-			accum += adder.add128(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128);
+			accum += virtualAdder.add128(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128);
 		}
 		return accum;
 	}
